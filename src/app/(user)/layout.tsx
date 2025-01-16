cfcda159b2fcc01dbd4dcff36d3993fbb364747c
bhargav <95672671+AS-PARTH@users.noreply.github.com>
2025-01-16 10:47:05 +0530

    Update layout.tsx

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -1,5 +1,5 @@
 import Navbar from "@/src/components/Navbar";
-import React, { useEffect } from "react";
+import React from "react";
 import "../style/globals.css";
 
 export const metadata = {
@@ -15,11 +15,41 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* <script
+        <link
+          rel="preload"
+          href="//script.crazyegg.com/pages/scripts/0127/1415.js"
+          as="script"
+          fetchPriority="high"
+        />
+        <link
+          rel="preload"
+          href="//script.crazyegg.com/pages/data-scripts/0127/1415/site/%SITE%.json?t=1"
+          as="fetch"
+          fetchPriority="high"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="//script.crazyegg.com/pages/data-scripts/0127/1415/sampling/%SITE%.json?t=1"
+          as="fetch"
+          fetchPriority="high"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="//script.crazyegg.com/pages/abtests-scripts/0127/1415/%SITE%.css"
+          as="style"
+          fetchPriority="high"
+        />
+        <link
+          href="//script.crazyegg.com/pages/abtests-scripts/0127/1415/%SITE%.css"
+          rel="stylesheet"
+        />
+        <script
           type="text/javascript"
-          src="//script.crazyegg.com/pages/scripts/0127/8659.js"
+          src="//script.crazyegg.com/pages/scripts/0127/1415.js"
           async
-        ></script> */}
+        ></script>
       </head>
       <body>
         <main id="ab-content">
