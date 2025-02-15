a9699552218dc2c1470ff618f30a8a18b375a858
bhargav <95672671+AS-PARTH@users.noreply.github.com>
2025-02-15 11:10:11 +0530

    Revert "Update layout.tsx"
    This reverts commit 9b54b74320a364dfcd5c2898a48f4415ecd52c8f.

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -16,6 +16,34 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="preload" href="/style/globals.css" as="style" />
+        <script
+          type="text/javascript"
+          src="//script.crazyegg.com/pages/scripts/0127/9507.js"
+          defer
+        ></script>
+        <script>
+          {`(function(w,d,s,l,i){
+            w[l]=w[l]||[];
+            w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+            var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),
+                dl=l!='dataLayer'?'&l='+l:'';
+            j.async=true;
+            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+            f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-WHGD2JXS');
+          
+          <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-HRE3GYM6YZ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-HRE3GYM6YZ');
+</script>
+          `}
+        </script>
       </head>
       <body>
         <noscript>
